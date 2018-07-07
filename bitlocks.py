083a99f8565365f8defef50a59304fe352ee157a
Li Bingdong <libingdong@Lis-MacBook-Pro.local>
2018-07-07 21:06:26 +0800

    End of a dream Back to English variables

--- bitlocks.py
+++ bitlocks.py
@@ -5,10 +5,8 @@ import math
 # import support_lib
 
 """
-what to solve
-more efficient communication protocols
-for example, FTP seems a good target
-Let us concentrate on encryption of it
+Data Encyrption + Image generator
+Yeah!
 """
 
 def random(length):
@@ -27,59 +25,31 @@ def factor(num):
             f.append(i)
     return f
 
-# def closeRatio(l1, l2, rat):
-#     # O(n^2)... Sorry
-#     cur_closest = 100000  # max
-#     l1_close_index = 0
-#     l2_close_index = 0
-#     outer = 1
-#     for i in range(0, len(l1)):
-#         for j in range(0, len(l2)):
-#             if (abs(float(l1[i])/float(l2[j]) - rat) < float(cur_closest)):
-#                 cur_closest = abs(float(l1[i])/float(l2[j]) - rat)
-#                 l1_close_index = i
-#                 l2_close_index = j
-#                 outer = 1
-#             if(abs(float(l2[j])/float(l1[i]) - rat) < float(cur_closest)):
-#                 cur_closest = abs(float(l2[j])/(l1[i]) - rat)
-#                 l1_close_index = i
-#                 l2_close_index = j
-#                 outer = 2
-#     return {
-#     "closest": cur_closest,
-#     "indices": [l1_close_index, l2_close_index],
-#     "originals": [l1, l2],
-#     "numbers": [l1[l1_close_index], l2[l2_close_index]],
-#     "reference": outer
-#     }
-
-def 大小获取神器(length):
+def get_size(length):
     return math.ceil(math.sqrt(length))
 
 def encodePlain(name, colorbase="red"):
     f = open("demo.txt", "r")
-    生的 = f.read()
-    熟的 = ""
-    随机数获取神器返回 = random(len(生的))
+    raw = f.read()
+    processed = ""
+    rand_res = random(len(raw))
     from PIL import Image
-    大小 = 大小获取神器(len(随机数获取神器返回))
-    img = Image.new('RGB', (大小, 大小))
-    图片数据 = []
-    for i in 随机数获取神器返回:
+    size = get_size(len(rand_res))
+    img = Image.new('RGB', (size, size))
+    img_dat = []
+    for i in rand_res:
         if colorbase == 'red':
-            像素 = (int(i)*25, 0, 0)
+            pixel = (int(i)*25, 0, 0)
         elif colorbase == "blue":
-            像素 = (0, 0, int(i)*25)
+            pixel = (0, 0, int(i)*25)
         elif colorbase == "green":
-            像素 = (0, int(i)*25, 0)
-        图片数据.append(像素)
-    img.putdata(图片数据)
+            pixel = (0, int(i)*25, 0)
+        img_dat.append(pixel)
+    img.putdata(img_dat)
     img.save('code.png')
-    # 字母表神器 = support_lib.Alphabet()
-    for 叶片位置 in range(0, len(生的)):
-        叶片 = 生的[叶片位置]
-        # 熟的 += 字母表神器.shift(随机数生成神器返回[叶片位置], 叶片)
-        熟的 += chr(ord(叶片) + int(随机数获取神器返回[叶片位置]))
-    return 熟的
+    for i in range(0, len(raw)):
+        itm = raw[i]
+        processed += chr(ord(itm) + int(rand_res[i]))
+    return processed
     f.close()
 print(encodePlain("demo.txt"))
